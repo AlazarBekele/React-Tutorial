9207cdab0f24c6850923af7ee15e437bb3d84b12
Alazar <alazarthe70@gmail.com>
2025-08-04 21:29:42 +0300

    Add Column Table

--- React-Tutorial/src/Message.tsx
+++ React-Tutorial/src/Message.tsx
@@ -10,6 +10,16 @@ function Message() {
 
             </div>
 
+            <div className="card">
+
+                <ul className="list-group list-group-flush">
+                    <li className="list-group-item">An item</li>
+                    <li className="list-group-item">A second item</li>
+                    <li className="list-group-item">A third item</li>
+                </ul>
+
+            </div>
+
         </div>
 
     )
